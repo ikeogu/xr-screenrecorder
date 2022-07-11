811512e90931bbf9c3fd90c4c38b05414d3b9ea3
ikeogu <ikeogu322@gmail.com>
2022-07-11 20:50:31 +0100

    my first commit

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue';
-import SnapRecordingVue from '../views/Snap-Recording.vue';
+import SnapRecordingVue from '@/views/Snap-Recording.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -13,9 +13,6 @@ const router = createRouter({
     {
       path: '/start-recording',
       name: 'start-recording',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
       component:SnapRecordingVue
     }
   ]
